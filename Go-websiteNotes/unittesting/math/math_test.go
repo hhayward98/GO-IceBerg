3b5d18f8909664819e26ba438ba60d7dc4b4a514
unknown <arkidenOT@outlook.com>
2022-08-26 11:55:24 -0600

    update

--- Go-websiteNotes/unittesting/math/math_test.go
+++ Go-websiteNotes/unittesting/math/math_test.go
@@ -1,10 +1,22 @@
 package math
 
 import (
-
+	"fmt"
 	"testing"
 
 )
+
+// arg1 + arg2 = expected
+type addTest struct {
+	arg1, arg2, expected int
+}
+
+var AddTest = []addTest {
+	addTest{5, 10, 15},
+	addTest{3, 8, 11},
+	addTest{30, 49, 79},
+
+}
 
 func Test1(t *testing.T) {
 
@@ -15,3 +27,24 @@ func Test1(t *testing.T) {
 		t.Errorf("got %q, wanted %q", got , want)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	for _, test := range AddTest {
+		if output := Add(test.arg1, test.arg2); output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
+	}
+}
+// when creating functions for Benchmark testting
+// Use "Benchmark" as prefix and use Function Name as suffix  
+func BenchmarkAdd(b *testing.B) {
+	for i :=0; i < b.N ; i++ {
+		Add(4, 6)
+	}
+}
+
+func ExampleAdd() {
+	fmt.Println(Add(8,2))
+
+}
+
