d216d7b26dc25f3fad2350e8f8fccc660264afa3
HHayward98 <arkidenOT@outlook.com>
2022-09-03 10:20:17 -0600

    update

--- Aesir/app.go
+++ Aesir/app.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"html/template"
+	_ "github.com/go-sql-driver/mysql"
 
 )
+
+
+var tpl *template.Template
 
 type InputForm struct {
 	name string
@@ -38,10 +43,25 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func Page2(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Running Page2\n")
 
-		//connect to database
+func Page3(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Running Page3\n")
+	tmpl := template.Must(template.ParseFiles("static/templates/page3.html")
+	
+	obj1 := favthings{r.FormValue("F1"), r.FormValue("F2"), r.FormValue("F3")}
+	fmt.Println(obj1)
+	obj2 := InputForm{r.FormValue("Uname"), r.FormValue("Color"), obj1}
+	fmt.Println(obj2)
+
+	tmpl.Execute(w, obj2)
+	log.Print("Running web-page")
+
+
+}
+
+func Page4(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("running Page4\n")
+
 	db, err := sql.Open("mysql", "Test:toor@(127.0.0.1:3308)/?parseTime=true")
 	Debugger(err)
 
@@ -53,44 +73,41 @@ func Page2(w http.ResponseWriter, r *http.Request) {
 	Debugger(err)
 
 	log.Print("Connected to DB")
-	
-	Sdata := favthings{
-		Thing1: r.FormValue("T1")
-		Thing2: r.FormValue("T2")
-		Thing3: r.FormValue("T3")
 
-	}
+	tpl.ExecuteTemplate(w, "Page4.html", "Connected To Database")
 
-	data := InputForm {
-		Name: r.FormValue("Uname")
-		Color: r.FormValue("Color")
-		Fthings: Sdata
-	}
+}
 
-	
-	
+
+
+func Page2(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Running Page2\n")
+
 	tmpl := template.Must(template.ParseFiles("static/templates/Page2.html"))
 	tmpl.Execute(w, "null")
-	log.Print("Running web-page")
 	return
 
 }
+
 
 func AppRoutes() {
 
 	//testing for docker container
 	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs)))
+	// fs := http.FileServer(http.Dir("static"))
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
 	
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/Page2", Page2)
+	http.HandleFunc("/Page3", Page3)
+	http.HandleFunc("/Page4", Page4)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
 
 
 func main() {
+	tpl, _ = template.ParseGlob("static/templates/*html")
 
 	log.Print("Listening......")
 	AppRoutes()
