ed973c1468fbf25ad17fbb2b87790cc336a42f0e
HHayward98 <arkidenOT@outlook.com>
2022-09-17 12:32:51 -0600

    postgreSQL Connect

---
+++ DB/PostgreSQl/DBConnect.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+
+	"database/sql"
+	"fmt"
+	 "github.com/lib/pq"
+
+)
+
+
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	password = "<password>"
+	dbname = "<dbname>"
+)
+
+
+func Check Error(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+
+
+func main () {
+	// connection string
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// connect to database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close Database connection
+	defer db.Close()
+
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+
+}
+
+
+
