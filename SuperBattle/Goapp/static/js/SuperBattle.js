97247f863fe6223c1d734d4348dd681e0ab7f069
HHayward98 <arkidenOT@outlook.com>
2022-10-20 14:56:41 -0600

    cleaned up

--- SuperBattle/Goapp/static/js/SuperBattle.js
+++ SuperBattle/Goapp/static/js/SuperBattle.js
@@ -5,16 +5,8 @@ function domLoaded() {
 	const DVbtn document.getElementById("Vdropbtn");
 	const DHbtn document.getElementById("Hdropbtn");
 
-	DHbtn.addEventListener("click", function()) {
-		console.log("Hero DropDown")
-	}
 
-	DVbtn.addEventListener("click", function()) {
-		console.log("Villain DropDown")
-	}
-
-
-}
+};
 
 
 
