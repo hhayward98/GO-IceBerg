64e650cea14ef0cd8aaa381f6de2737dea9a6a87
HHayward98 <arkidenOT@outlook.com>
2023-01-17 11:12:31 -0700

    Some unit Test

--- HuntersWebsite/webapp/main_test.go
+++ HuntersWebsite/webapp/main_test.go
@@ -1,11 +1,106 @@
-package main
+package main 
 
 import (
-
-	"fmt"
-	"testing"
+	"bytes"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"testing"
+	// "github.com/rs/cors"
 
 )
 
 
+
+func TestGetHome(t *testing.T) {
+
+	res, err := http.Get("http://localhost:8080/Home")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	// ByteBuffer, err := ioutil.ReadAll(res.Body)
+
+	// got := string(ByteBuffer)
+
+	// log.Println("response data: ",got)
+
+	// want := 
+
+	// if got != want {
+	// 	t.Errorf("got %q, wanted %q", got , want)
+	// }
+
+}
+
+
+func TestPostHome(t *testing.T) {
+
+	res, err := http.Post("http://localhost:8080/Home", "application/json",  bytes.NewBuffer([]byte("{\"Message\": \"Test\"}")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	ByteBuffer, err := ioutil.ReadAll(res.Body)
+
+	got := string(ByteBuffer)
+
+	// log.Println("response data: ",got)
+
+	want := "Method Not Allowed\n"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got , want)
+	}
+
+}
+
+func TestPostProjects(t *testing.T) {
+
+
+	res, err := http.Post("http://localhost:8080/Projects", "application/json",  bytes.NewBuffer([]byte("{\"Message\": \"Test\"}")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	ByteBuffer, err := ioutil.ReadAll(res.Body)
+
+	got := string(ByteBuffer)
+
+	// log.Println("response data: ",got)
+
+	want := "Method Not Allowed\n"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got , want)
+	}
+
+
+}
+
+
+func TestPostSkills(t *testing.T) {
+
+	res, err := http.Post("http://localhost:8080/Skills", "application/json", bytes.NewBuffer([]byte("{\"Message\": \"Test\"}")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	ByteBuffer, err := ioutil.ReadAll(res.Body)
+
+	got := string(ByteBuffer)
+
+	want := "Method Not Allowed\n"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	
+	}
+
+	
+}
